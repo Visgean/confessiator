50f8311b49bb8fb118ae6d73409d5ee373f819c6
Visgean Skeloru <visgean@gmail.com>
2013-04-16 01:58:40 +0200

    debugging production settings

--- confessiator/settings/production.py
+++ confessiator/settings/production.py
@@ -11,16 +11,16 @@ import urlparse
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
 
-location = os.path.join(os.environ['OPENSHIFT_REPO_DIR'])
+location = os.path.join(os.environ['OPENSHIFT_REPO_DIR'], 'confessiator')
 
 TEMPLATE_DIRS = (
-	os.path.join(PROJECT_DIR, "templates"),
-	# here you can add another templates directory if you wish.
+    os.path.join(location, "templates"),
+    # here you can add another templates directory if you wish.
 )
 
 
 sys.path.append(location)
-sys.path.append(location + 'confessiator')
+sys.path.append(location + '..')
 
 MEDIA_ROOT = location + "media/"
 
@@ -29,33 +29,35 @@ TEMPLATE_DIRS = (location + "templates/",)
 
 EMAIL_HOST = 'localhost'
 
-DATABASES = {}
+DATABASES = {'default':
+            {
+                'ENGINE': 'django.db.backends.sqlite3',
+                'NAME': 'dev.db',
+                'USER': '',
+                'PASSWORD': '',
+                'HOST': '',
+                'PORT': '',
+            },
+}
+
+
 if 'OPENSHIFT_MYSQL_DB_URL' in os.environ:
-	url = urlparse.urlparse(os.environ.get('OPENSHIFT_MYSQL_DB_URL'))
-	DATABASES['default'] = {
-		'ENGINE': 'django.db.backends.mysql',
-		'NAME': os.environ['OPENSHIFT_APP_NAME'],
-		'USER': url.username,
-		'PASSWORD': url.password,
-		'HOST': url.hostname,
-		'PORT': url.port,
-	}
+    url = urlparse.urlparse(os.environ.get('OPENSHIFT_MYSQL_DB_URL'))
+    DATABASES['default'] = {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': os.environ['OPENSHIFT_APP_NAME'],
+        'USER': url.username,
+        'PASSWORD': url.password,
+        'HOST': url.hostname,
+        'PORT': url.port,
+    }
 elif 'OPENSHIFT_POSTGRESQL_DB_URL' in os.environ:
-	url = urlparse.urlparse(os.environ.get('OPENSHIFT_POSTGRESQL_DB_URL'))
-	DATABASES['default'] = {
-		'ENGINE': 'django.db.backends.postgresql_psycopg2',
-		'NAME': os.environ['OPENSHIFT_APP_NAME'],
-		'USER': url.username,
-		'PASSWORD': url.password,
-		'HOST': url.hostname,
-		'PORT': url.port,
-	}
-else:
-	DATABASES['default'] = {
-		'ENGINE': 'django.db.backends.sqlite3',
-		'NAME': 'dev.db',
-		'USER': '',
-		'PASSWORD': '',
-		'HOST': '',
-		'PORT': '',
-	}
+    url = urlparse.urlparse(os.environ.get('OPENSHIFT_POSTGRESQL_DB_URL'))
+    DATABASES['default'] = {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': os.environ['OPENSHIFT_APP_NAME'],
+        'USER': url.username,
+        'PASSWORD': url.password,
+        'HOST': url.hostname,
+        'PORT': url.port,
+    }
