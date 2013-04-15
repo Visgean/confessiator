29fa49e3c36ae75a9718cc5b83bf1363464689dc
Visgean Skeloru <visgean@gmail.com>
2013-04-16 01:27:21 +0200

    added new structure to the project

--- wsgi.py
+++ wsgi.py
@@ -1,24 +1,24 @@
-from django.core.wsgi import get_wsgi_application
-application = get_wsgi_application()
-
-
 #!/usr/bin/env python
- 
-# some original codes we need
+ 
 import os
- 
-virtenv = os.environ['APPDIR'] + '/virtenv/'
-os.environ['PYTHON_EGG_CACHE'] = os.path.join(virtenv, 'lib/python2.6/site-packages')
+import sys
+ 
+sys.path.append(os.path.join(os.environ['OPENSHIFT_REPO_DIR']))
+ 
+os.environ['DJANGO_SETTINGS_MODULE'] = 'confessiator.settings.base'
+ 
+virtenv = os.environ['OPENSHIFT_HOMEDIR'] + 'python-2.7/virtenv/'
+os.environ['PYTHON_EGG_CACHE'] = os.path.join(virtenv, 'lib/python2.7/site-packages')
+ 
 virtualenv = os.path.join(virtenv, 'bin/activate_this.py')
 try:
-    execfile(virtualenv, dict(__file__=virtualenv))
-except:
-    pass
- 
-# new codes we adding for Django
-import sys
+    execfile(virtualenv, dict(__file__=virtualenv))
+except IOError:
+    pass
+#
+# IMPORTANT: Put any additional includes below this line.  If placed above this
+# line, it's possible required libraries won't be in your searchable path
+#
+ 
 import django.core.handlers.wsgi
- 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "confessiator.settings.base")
-sys.path.append(os.path.join(os.environ['OPENSHIFT_REPO_DIR'], 'wsgi' ))
 application = django.core.handlers.wsgi.WSGIHandler()
