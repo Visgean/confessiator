dc074bb4109ca8a5033f3dd1b6fef7123e6e6e4f
Visgean Skeloru <visgean@gmail.com>
2013-04-16 00:19:30 +0200

    wsgi for openshift

--- wsgi.py
+++ wsgi.py
@@ -1,16 +1,24 @@
-
-import os
-import sys, getpass, os.path
-
-
-location = os.path.dirname(__file__) # this is not Django setting.)
-
-
-sys.path.append(location)
-sys.path.append(location + '../')
-
-
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "confessiator.settings.base")
-
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
+
+
+#!/usr/bin/env python
+ 
+# some original codes we need
+import os
+ 
+virtenv = os.environ['APPDIR'] + '/virtenv/'
+os.environ['PYTHON_EGG_CACHE'] = os.path.join(virtenv, 'lib/python2.6/site-packages')
+virtualenv = os.path.join(virtenv, 'bin/activate_this.py')
+try:
+    execfile(virtualenv, dict(__file__=virtualenv))
+except:
+    pass
+ 
+# new codes we adding for Django
+import sys
+import django.core.handlers.wsgi
+ 
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "confessiator.settings.base")
+sys.path.append(os.path.join(os.environ['OPENSHIFT_REPO_DIR'], 'wsgi' ))
+application = django.core.handlers.wsgi.WSGIHandler()
