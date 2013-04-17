2cdb0ab34a6751b473e5c8b8d0141d058a2a2834
Visgean Skeloru <visgean@gmail.com>
2013-04-17 23:02:08 +0200

    Post only three objects per hour

--- confessiator/secrets/management/commands/confess.py
+++ confessiator/secrets/management/commands/confess.py
@@ -6,7 +6,7 @@ class Command(BaseCommand):
 	help = 'Tries to post all the approved confessions'
 
 	def handle(self, *args, **options):
-		for c in Confession.objects.filter(approved=True, posted = False)[:5]:
+		for c in Confession.objects.filter(approved=True, posted = False)[:3]:
 			api = c.wall.get_graph_api()
 			message = api.put_object(c.wall.facebook_id, "feed", message=c.content.encode('utf-8'))
 			c.posted = True
