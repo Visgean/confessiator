1bfc0540188bb30a8fd1b6035b46d2f3b6c4a5a6
Visgean Skeloru <visgean@gmail.com>
2013-04-18 09:29:13 +0200

    chech if the message was already posted

--- confessiator/secrets/management/commands/confess.py
+++ confessiator/secrets/management/commands/confess.py
@@ -8,6 +8,13 @@ class Command(BaseCommand):
 	def handle(self, *args, **options):
 		for c in Confession.objects.filter(approved=True, posted = False)[:3]:
 			api = c.wall.get_graph_api()
-			message = api.put_object(c.wall.facebook_id, "feed", message=c.content.encode('utf-8'))
-			c.posted = True
+
+			fql = u'SELECT message FROM stream WHERE source_id={0} AND message="{1}"'.format(c.wall.facebook_id, c.content)
+			posted = bool(c.wall.user.get_graph_api.fql(fql.encode('utf-8')))
+			if posted:
+				c.posted=True
+			else:
+				message = api.put_object(c.wall.facebook_id, "feed", message=c.content.encode('utf-8'))
+				c.posted = True
+		
 			c.save()
