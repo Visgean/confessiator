f441eeefd651a6c2391203da9fd7c2efe493732a
Visgean Skeloru <visgean@gmail.com>
2013-04-19 00:52:04 +0200

    print error thing

--- confessiator/secrets/management/commands/test_posting.py
+++ confessiator/secrets/management/commands/test_posting.py
@@ -8,6 +8,11 @@ class Command(BaseCommand):
 
 	def handle(self, *args, **options):
 		for c in Confession.objects.filter(approved=True, posted = True):
+			api = c.wall.get_graph_api()
+
 			fql = u"""SELECT message FROM stream WHERE source_id={0} AND message='{1}' """.format(c.wall.facebook_id, c.content)
-			posted = c.wall.owner.graph_api.fql(fql.encode('utf-8')) # we have to use users api as wall api cannot do fql
-			print posted
+			try:
+				posted = c.wall.owner.graph_api.fql(fql.encode('utf-8')) # we have to use users api as wall api cannot do fql
+				print posted
+			except:
+				print post
