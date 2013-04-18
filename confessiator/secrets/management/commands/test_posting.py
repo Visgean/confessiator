c383dbc6981526f84d1554115e3f4d6c47654c3d
Visgean Skeloru <visgean@gmail.com>
2013-04-19 00:44:23 +0200

    test posting command

---
+++ confessiator/secrets/management/commands/test_posting.py
@@ -0,0 +1,13 @@
+from django.core.management.base import BaseCommand, CommandError
+from secrets.models import Confession
+from facebook import GraphAPIError
+from ssl import SSLError
+
+class Command(BaseCommand):
+	help = 'Tries to post all the approved confessions'
+
+	def handle(self, *args, **options):
+		for c in Confession.objects.filter(approved=True, posted = True):
+			fql = u"""SELECT message FROM stream WHERE source_id={0} AND message='{1}' """.format(c.wall.facebook_id, c.content)
+			posted = c.wall.owner.graph_api.fql(fql.encode('utf-8')) # we have to use users api as wall api cannot do fql
+			print posted
