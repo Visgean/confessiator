1ced2e8d7c8d9307e781661cde34fc70ca4781a8
Visgean Skeloru <visgean@gmail.com>
2013-04-18 23:40:20 +0200

    Catching ssl error

--- confessiator/secrets/management/commands/confess.py
+++ confessiator/secrets/management/commands/confess.py
@@ -1,6 +1,7 @@
 from django.core.management.base import BaseCommand, CommandError
 from secrets.models import Confession
 from facebook import GraphAPIError
+from ssl import SSLError
 
 class Command(BaseCommand):
 	help = 'Tries to post all the approved confessions'
@@ -9,16 +10,17 @@ class Command(BaseCommand):
 		for c in Confession.objects.filter(approved=True, posted = False)[:3]:
 			api = c.wall.get_graph_api()
 
-			# fql = u"""SELECT message FROM stream WHERE source_id={0} AND message="{1}" """.format(c.wall.facebook_id, c.content)
-			# posted = c.wall.owner.graph_api.fql(fql.encode('utf-8')) # we have to use users api as wall api cannot do fql
-			# if posted:
-			# 	c.posted=True
-			# else:
+		fql = u"""SELECT message FROM stream WHERE source_id={0} AND message='{1}' """.format(c.wall.facebook_id, c.content)
+		posted = c.wall.owner.graph_api.fql(fql.encode('utf-8')) # we have to use users api as wall api cannot do fql
+		print posted
+
 		try:
 			message = api.put_object(c.wall.facebook_id, "feed", message=c.content.encode('utf-8'))
 			c.posted = True
 		except GraphAPIError, e:
 			if e == '(#506) Duplicate status message':
 				c.posted = True
+		except SSLError:
+			c.posted = True
 
-			c.save()
+		c.save()
