22e35f94166068abee045c8380d54aa059c6f145
Visgean Skeloru <visgean@gmail.com>
2013-04-19 00:33:32 +0200

    Oh, the cycles!

--- confessiator/secrets/management/commands/confess.py
+++ confessiator/secrets/management/commands/confess.py
@@ -10,17 +10,23 @@ class Command(BaseCommand):
 		for c in Confession.objects.filter(approved=True, posted = False)[:3]:
 			api = c.wall.get_graph_api()
 
-		fql = u"""SELECT message FROM stream WHERE source_id={0} AND message='{1}' """.format(c.wall.facebook_id, c.content)
-		posted = c.wall.owner.graph_api.fql(fql.encode('utf-8')) # we have to use users api as wall api cannot do fql
-		print posted
+			fql = u"""SELECT message FROM stream WHERE source_id={0} AND message='{1}' """.format(c.wall.facebook_id, c.content)
+			try:
+				posted = c.wall.owner.graph_api.fql(fql.encode('utf-8')) # we have to use users api as wall api cannot do fql
+				if posted: 
+					c.posted = True
+					c.save()
+					continue # lets not do anuthing with that
+			except:
+				pass
 
-		try:
-			message = api.put_object(c.wall.facebook_id, "feed", message=c.content.encode('utf-8'))
-			c.posted = True
-		except GraphAPIError, e:
-			if e == '(#506) Duplicate status message':
+			try:
+				message = api.put_object(c.wall.facebook_id, "feed", message=c.content.encode('utf-8'))
 				c.posted = True
-		except SSLError:
-			c.posted = True
+			except GraphAPIError, e:
+				if e == '(#506) Duplicate status message':
+					c.posted = True
+			except SSLError:
+				c.posted = True
 
-		c.save()
+			c.save()
